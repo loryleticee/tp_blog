1cad1c08caf6d18cfc4530a20aff6de3ce59d1da
loryleticee <loryleticee@gmail.com>
2021-12-03 00:00:31 +0100

    add some comment to index page api

--- index.php
+++ index.php
@@ -1,2 +1,26 @@
 <?php
-print(json_encode("WELCOME TO THE API OF BLOG DES TITANS"));
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json; charset=utf-8');
+
+$routes = [
+    "Accueil" => "WELCOME TO THE API OF BLOG DES TITANS",
+    "routes" => [
+        "/api/getArticles" => [
+            "description" => "GET ALL ARTICLES"
+        ],
+        "/api/postArticle" => [
+            "description" => "ADD AN ARTICLE"
+        ],
+        "/api/putArticle" => [
+            "description" => "MODIFY AN ARTICLE"
+        ],
+        "/api/getCategories" => [
+            "description" => "GET ALL CATEGORIES"
+        ],
+        "/api/postCategorie" => [
+            "description" => "ADD A CATEGORIE"
+        ],
+    ]
+    ];
+
+print(json_encode($routes));
