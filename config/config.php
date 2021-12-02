3bc899c884b1399ea24977e6e312d73a73e1249d
loryleticee <loryleticee@gmail.com>
2021-12-02 20:39:20 +0100

    header json

--- config/config.php
+++ config/config.php
@@ -1,6 +1,8 @@
 <?php
 $domaine = "http://localhost:8888";
 header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json; charset=utf-8');
+
 $error = [
     "message" => "",
     "exist" => false
