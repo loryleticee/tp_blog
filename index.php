72af83f9484fcdb220bb08a5a5dc4a55e685b9b8
loryleticee <loryleticee@gmail.com>
2021-12-03 10:11:34 +0100

    add method to documentation api

--- index.php
+++ index.php
@@ -6,35 +6,60 @@ $routes = [
     "Accueil" => "WELCOME TO THE API OF BLOG DES TITANS",
     "routes" => [
         "/api/getArticles.php" => [
+            "METHOD" => "GET",
             "description" => "GET ALL ARTICLES",
-            "required params" => []
+            "required params" => [],
+            "return" => [
+                "on_success" => "array of object datas",
+                "on_failure" => "Object with a key (string) 'message' and a key (bool) 'exist' ",
+            ]
         ],
         "/api/postArticle.php" => [
+            "METHOD" => "POST",
             "description" => "ADD AN ARTICLE",
             "required params" => [
                 "categorie" => "L'identifiant de la catégorie de l'article",
                 "content" => "Le contenu de l'article",
                 "title" => "Le titre de l'article",
                 "user_id" => "L'identifiant de l'utilisateur propriétaire de l' article",
+            ],
+            "return" => [
+                "on_success" => "array of object datas with the new post id",
+                "on_failure" => "Object with a key (string) 'message' and a key (bool) 'exist' ",
             ]
         ],
         "/api/putArticle.php" => [
+            "METHOD" => "POST",
             "description" => "MODIFY AN ARTICLE",
             "required params" => [
                 "article_id" => "L'identifiant de l'article",
                 "title" => "Le titre de l'article",
                 "content" => "Le contenu de l'article",
                 "user_id" => "L'identifiant de l'utilisateur propriétaire de l' article",
+            ],
+            "return" => [
+                "on_success" => "array of object datas",
+                "on_failure" => "Object with a key (string) 'message' and a key (bool) 'exist' ",
             ]
         ],
         "/api/getCategories.php" => [
+            "METHOD" => "GET",
             "description" => "GET ALL CATEGORIES",
             "required params" => [],
+            "return" => [
+                "on_success" => "array of object datas",
+                "on_failure" => "Object with a key (string) 'message' and a key (bool) 'exist' ",
+            ]
         ],
         "/api/postCategorie.php" => [
+            "METHOD" => "POST",
             "description" => "ADD A CATEGORIE",
             "required params" => [
                 "label" => "Le libellé de la catégorie",
+            ],
+            "return" => [
+                "on_success" => "array of object datas with the new categorie id",
+                "on_failure" => "Object with a key (string) 'message' and a key (bool) 'exist' ",
             ]
         ],
     ]
