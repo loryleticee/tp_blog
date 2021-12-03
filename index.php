345afe51948abe88e2a2f5aff9dec89cc7f91ebf
loryleticee <loryleticee@gmail.com>
2021-12-03 06:12:11 +0100

    add routes description

--- index.php
+++ index.php
@@ -5,22 +5,39 @@ header('Content-Type: application/json; charset=utf-8');
 $routes = [
     "Accueil" => "WELCOME TO THE API OF BLOG DES TITANS",
     "routes" => [
-        "/api/getArticles" => [
-            "description" => "GET ALL ARTICLES"
+        "/api/getArticles.php" => [
+            "description" => "GET ALL ARTICLES",
+            "required params" => []
         ],
-        "/api/postArticle" => [
-            "description" => "ADD AN ARTICLE"
+        "/api/postArticle.php" => [
+            "description" => "ADD AN ARTICLE",
+            "required params" => [
+                "categorie" => "L'identifiant de la catégorie de l'article",
+                "content" => "Le contenu de l'article",
+                "title" => "Le titre de l'article",
+                "user_id" => "L'identifiant de l'utilisateur propriétaire de l' article",
+            ]
         ],
-        "/api/putArticle" => [
-            "description" => "MODIFY AN ARTICLE"
+        "/api/putArticle.php" => [
+            "description" => "MODIFY AN ARTICLE",
+            "required params" => [
+                "article_id" => "L'identifiant de l'article",
+                "title" => "Le titre de l'article",
+                "content" => "Le contenu de l'article",
+                "user_id" => "L'identifiant de l'utilisateur propriétaire de l' article",
+            ]
         ],
-        "/api/getCategories" => [
-            "description" => "GET ALL CATEGORIES"
+        "/api/getCategories.php" => [
+            "description" => "GET ALL CATEGORIES",
+            "required params" => [],
         ],
-        "/api/postCategorie" => [
-            "description" => "ADD A CATEGORIE"
+        "/api/postCategorie.php" => [
+            "description" => "ADD A CATEGORIE",
+            "required params" => [
+                "label" => "Le libellé de la catégorie",
+            ]
         ],
     ]
-    ];
+];
 
 print(json_encode($routes));
